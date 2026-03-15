6cc7e1ca32520bc321bd079ee81e3068ad459e81
DimebagULKU <denizulku2011@gmail.com>
2026-03-15 05:33:01 +0300

    Change PowerShell command to start process in hidden mode

--- NEXA/run-hidden.bat
+++ NEXA/run-hidden.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-powershell -WindowStyle Hidden -Command "Start-Process 'mine_nexa_with_oc.bat'"
+powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c mine_nexa_with_oc.bat' -WindowStyle Hidden"
