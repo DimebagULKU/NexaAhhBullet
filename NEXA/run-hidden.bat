9a165538dfbd88690f3973c9eda3b0654517fd8f
DimebagULKU <denizulku2011@gmail.com>
2026-03-15 05:36:13 +0300

    Modify PowerShell command to include -FilePath and -Wait

--- NEXA/run-hidden.bat
+++ NEXA/run-hidden.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c mine_nexa_with_oc.bat' -WindowStyle Hidden"
+powershell -WindowStyle Hidden -Command "Start-Process -FilePath cmd -ArgumentList '/c mine_nexa_with_oc.bat' -WindowStyle Hidden -Wait"
